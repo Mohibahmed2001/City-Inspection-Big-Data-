aa02380473f7e46f1ba296cb921aaa88de41aeb2
JannatunNazifa <72843996+JannatunNazifa@users.noreply.github.com>
2024-04-01 18:36:48 -0400

    Add files via upload

---
+++ dataset/Brooklyn_Bronx.py
@@ -0,0 +1,42 @@
+import pymongo
+import random
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")  
+db = client["city_inspections_db"] 
+collection = db["inspections"]  
+
+
+violation_criteria = "Violation Issued"
+
+cities = ["BROOKLYN", "BRONX"]
+
+violations_count = collection.count_documents({
+    "address.city": {"$in": cities},
+    "result": violation_criteria
+})
+
+print(f"Total business violations in Brooklyn and Bronx: {violations_count}") 
+print()
+
+businesses_in_brooklyn = collection.find({"address.city": "BROOKLYN","result": violation_criteria})
+brooklyn_count = collection.count_documents({"address.city": "BROOKLYN","result": violation_criteria})
+businesses_in_bronx = collection.find({"address.city": "BRONX","result": violation_criteria})
+bronx_count = collection.count_documents({"address.city": "BRONX","result": violation_criteria})
+diff= brooklyn_count - bronx_count
+
+print("First five businesses located in Brooklyn:")
+for business in businesses_in_brooklyn[:5]:
+    print(business["business_name"])
+    
+print()
+
+print("First five businesses located in Bronx:")
+for business in businesses_in_bronx[:5]:
+    print(business["business_name"])
+    
+print()
+print(f"Total number of violations in Brooklyn: {brooklyn_count}")
+print(f"Total number of violations in Bronx: {bronx_count}")
+print(f"Difference in count: {diff}")
+    
+
