fa7c177a228d99a9ad05db672a9fd14424aff8e8
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-04-07 08:42:19 -0400

    Update zipcode.py

--- dataset/zipcode.py
+++ dataset/zipcode.py
@@ -1,24 +1,28 @@
 import pymongo
-import random
+from random import sample
 
+# Conneect to database
 client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["city_inspections_db"]
-collection = db["inspections"]
+db = client["cityinspections"]
+collection = db["names"]
 
-json_file_path = "C:/Users/mohibahmed/Desktop/mongodb-json-files-master/datasets/city_inspections.json"
-
-
-# Ask user for zip code
 zip_code = input("Enter a zip code: ")
-businesses_in_zip = collection.find({"address.zip": zip_code})
 
-# List buisnesses that are found
-if businesses_in_zip.count() > 0:
-    total_businesses = businesses_in_zip.count()
+# Find buisnees in zipcode and count them
+businesses_in_zip = collection.find({"address.zip": int(zip_code)})
+total_businesses = collection.count_documents({"address.zip": int(zip_code)})
+
+if total_businesses > 0:
     print(f"Total number of businesses in zip code {zip_code}: {total_businesses}")
-    selected_businesses = random.sample(list(businesses_in_zip), min(5, total_businesses))
-    print("Randomly selected businesses:")
+    
+    # Select 5 random businesses and print them
+    selected_businesses = sample(list(businesses_in_zip), min(5, total_businesses))   
+    print(f"Randomly selected businesses in zip code {zip_code}:")
     for business in selected_businesses:
-        print(business["business_name"])
+        print(business['business_name'])
+        
 else:
     print("Zip code not found.")
+    
+
+client.close()
